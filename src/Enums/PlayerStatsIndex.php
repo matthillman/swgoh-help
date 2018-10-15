75d928eb464807c7f3a48aa569fb0f6d47ada6b8
Matthew Hillman <mhillman@bomgar.com>
2018-10-14 21:20:04 -0500

    Add index enum

---
+++ src/Enums/PlayerStatsIndex.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace SwgohHelp\Enums;
+
+use MyCLabs\Enum\Enum;
+
+class PlayerStatsIndex extends Enum {
+    const gp = 1;
+    const charGP = 2;
+    const shipGP = 3;
+    const fleetWon = 4;
+    const arenaWon = 5;
+    const pveWon = 6;
+    const hardWon = 7;
+    const gwWon = 8;
+    const raidsWon = 9;
+    const guildTokens = 10;
+    const donated = 11;
+}
